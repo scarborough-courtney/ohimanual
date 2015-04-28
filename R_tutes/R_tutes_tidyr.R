2a0b0277ff89d918cccf350b7d5ae108c25324a1
Casey O'Hara <ohara@nceas.ucsb.edu>
2015-04-28 13:50:32 -0700

    added examples

--- R_tutes/R_tutes_tidyr.R
+++ R_tutes/R_tutes_tidyr.R
@@ -1,7 +1,11 @@
 ### if not already installed, install these OHI favorites:
-install.packages(c('readr', 'stringr', 'tidyr', 'dplyr'))
+ohi_packs <- c('readr', 'stringr', 'tidyr', 'dplyr')
+for (pack in ohi_packs) {
+ if (!require(pack, character.only = TRUE)) 
+   install.packages(pack)
+}
 
-### once installed, attach the packages from the library:
+### once installed, you only need to attach the packages from the library:
 library(readr)
 library(stringr)
 library(tidyr)
@@ -15,11 +19,11 @@ library(dplyr)
 ### download FAO commodity sample data and OHI lookup table:
 ### * https://github.com/OHI-Science/ohimanual/blob/masterR_tutes/data/commodities_sample.csv
 ### * https://github.com/OHI-Science/ohimanual/blob/masterR_tutes/data/comm2prod_sample.csv
-dir_data <- './R_tutes/data'  ### wherever you saved your files
+dir_data  <- './R_tutes/data'  ### wherever you saved your files
 
-data     <- read.csv(sprintf('%s/commodities_sample.csv', dir_data))
+data_wide <- read.csv(sprintf('%s/commodities_sample.csv', dir_data))
   ### harvest data on marine commodities (FAO)
-lookup   <- read.csv(sprintf('%s/comm2prod_sample.csv',   dir_data))
+lookup    <- read.csv(sprintf('%s/comm2prod_sample.csv',   dir_data))
   ### an OHI lookup table to assign individual commodities into product groups
 
 ##############################################################/
@@ -30,13 +34,23 @@ lookup   <- read.csv(sprintf('%s/comm2prod_sample.csv',   dir_data))
 ### and each row is a single observation.
 data_long <- data_wide %>%
   gather(year, tonnes, -Country, -Commodity, -Trade)
-### Note that the (-Country, -Commodity, -Trade) arguments tell the
-### function to gather all except those rows while gathering, and instead
-### repeat these values for each of the observations in the new 'year'
-### column.
+    ### Note that the (-Country, -Commodity, -Trade) arguments tell the
+    ### function to gather all except those rows while gathering, and instead
+    ### repeat these values for each of the observations in the new 'year'
+    ### column.    
+
 ### The same result could be achieved with this alternative function call:
 data_long1 <- data_wide %>%
   gather(year, tonnes, X2007:X2011)
+
+### Note that you might get a warning message: "attributes are not
+### identical across measure variables; they will be dropped".  If you
+### inspect the original data_wide table, some columns ('X2009') are all
+### integer values, and others ('X2008') contain a mix of numbers and
+### letters (e.g. '...' or '0 0').  R tries to figure out what type of
+### data is in each column, but when faced with complex data, it has to
+### resort to more flexible data classes, e.g. character.
+
 
 ### Spread data (less commonly used)
 ### Transform data from long format (columns as variables) into wide
@@ -71,29 +85,39 @@ harvest <- harvest %>%
       # into character, and then as.integer() and as.numeric converts
       # characters into integers/numbers where possible.
 
-### use group_by() to summarize harvest information by commodity for each country
-harvest <- harvest %>%
-  group_by(country, commodity)
 
-h_tot_sum <- harvest %>%
-  summarize(harvest_tot = sum(tonnes, na.rm = TRUE))
 ### Summarize information by collapsing each group to just a single
-###   summary value (total tonnage by commodity by country)
+### summary value (total tonnage by commodity by country).
+### Use group_by() to summarize harvest information by commodity for 
+### each country. When done, it's a good idea to use ungroup().
+h_tot_sum <- harvest %>%
+  group_by(country, commodity) %>%
+  summarize(harvest_tot = sum(tonnes, na.rm = TRUE)) %>%
+  ungroup()
 
-h_tot_mut <- harvest %>%
-  mutate(harvest_tot = sum(tonnes, na.rm = TRUE)) %>%
-  arrange(country, commodity)
 ### Summarize information by creating a new variable to contain summary
 ###   value; report value for every observation
+h_tot_mut <- harvest %>%
+  group_by(country, commodity) %>%
+  mutate(harvest_tot = sum(tonnes, na.rm = TRUE)) %>%
+  ungroup() %>%
+  arrange(country, commodity)
+
+### use desc() in arrange to sort in reverse order
+harvest_reverse <- h_tot_mut %>%
+  arrange(desc(year), country, desc(commodity))
+    ### sorted by year newer-to-older, and then within each year 
+    ### by country in alpha order, then within each country/year
+    ### by commodity in reverse alpha order.
 
 ##############################################################/
 ### %>% operator examples ====
 ###
 ### Ugly nested functions:
 h_recent_totals <-arrange(mutate(filter(group_by(harvest, country, commodity),
-year >= 2009), harvest_tot = sum(tonnes, na.rm = TRUE)), country, commodity)
+  year >= 2009), harvest_tot = sum(tonnes, na.rm = TRUE)), country, commodity)
 
-### Boring sequential functions:
+### Boring and unintuitive sequential functions:
 h_temp <- group_by(harvest, country, commodity)
 h_temp <- filter(h_temp, year >= 2009)
 h_temp <- mutate(h_temp, harvest_tot = sum(tonnes, na.rm = TRUE))
